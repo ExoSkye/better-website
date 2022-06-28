4227ddb7b67b8cdf27c7cec44c89e8bbf07acdd0
ProtoByter <bobrow2006@gmail.com>
2022-06-28 07:30:44 +0000

    Do stuff

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
+    plugins: [require("@tailwindcss/typography"), require('daisyui')],
+    daisyui: {
+        themes: ["synthwave"],
+    },
+};
+  
